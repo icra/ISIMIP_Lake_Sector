5a3244f5a7daee4f7584d1f0f524fe52ec0ec7a4
dmercado@icra.cat <dmercado@icra.cat>
2022-02-11 13:01:46 +0100

    Code modified

--- Calculate_lakedepth.R
+++ Calculate_lakedepth.R
@@ -4,12 +4,12 @@
 ##
 ## Purpose of script: To calculate lake depth from HydroLAKES v1.0 database
 ##
-## Author: Daniel Mercado-Bettín and Rafael Marcé
+## Authors: Daniel Mercado-Bettín and Rafael Marcé
 ## Institute: Catalan Institute for Water Research (ICRA)
 ## 
-## Date Created: 2022-02-10
+## Date Created: 2022-02-11
 ## 
-## Email: dmercado@icra.cat
+## Email: dmercado@icra.cat, rmarce@icra.cat
 ##
 ## ---------------------------
 ##
@@ -37,13 +37,24 @@ weighted_median <- function(x, w){
   out <- approx(Fx, x_o, xout=probs, ties="ordered", rule=2,
                 method="linear")
   
-  closest_pos <- which(abs(out$y-x)==min(abs(out$y-x))) #position of the closest value
+  closest_pos <- which(abs((out$y-x)/x)==min(abs((out$y-x)/x))) #position of the closest value
   if (length(closest_pos)>1){
     closest_pos_area <- which(w[closest_pos]==max(w[closest_pos])) #lake with greater area selected, in case there are more than one depths with the same value
     closest_pos <- closest_pos[closest_pos_area][1]
   }
   result <- c(x[closest_pos], closest_pos) #saving weighted median depth and its position in initial 'x' vector
   return(result)
+}
+
+## Function to calculate real areas of each pixel according to latitude degree (Earth distortion)
+#This is needed to select the big lakes for each pixel
+areakm2lat <- function(latitude, resolution=0.5, R=6371007){
+  # R=6371007m is the authalic earth radius at equator
+  
+  height <- resolution*pi/180*R #height of the cells, same value for the whole grid
+  width <- (sin((latitude+resolution/2)*pi/180)-sin((latitude-resolution/2)*pi/180))*R #cells width
+  a_km2 <- width*height/1e6 #cells area depending on latitude
+  return(a_km2)
 }
 
 ## opening HydroLAKES
@@ -57,6 +68,7 @@ HL_cent <- st_point_on_surface(HL_sf)
 HL_df <- data.frame(st_drop_geometry(HL_cent), st_coordinates(HL_cent))
 
 ## filling new matrix for each longitude and latitude with the weighted median values
+#empty matrix to save all weighted median map for depth
 wm_matrix <- matrix(data = NA, nrow=360, ncol = 720) #empty matrix to save all weighted median map for depth
 
 #empty matrix to save all weighted median map for the rest of the data in HydroLAKES
@@ -65,6 +77,9 @@ for (v in 1:length(var_vector)){ #vector of data to be saved from HydroLAKES dat
   c <- c+1
   wm_list[[c]] <- matrix(data = NA, nrow=360, ncol = 720)
 } 
+#empty matrix to save all position of the big lakes > 0.5 degrees
+wm_biglakes <- matrix(data = NA, nrow=360, ncol = 720) #empty matrix to save all weighted median map for depth
+
 c_lon <- 0; 
 for (lon in seq(-180, 179.5, 0.5)){ #loop in longitude
   c_lon <- c_lon+1; c_lat <- 0
@@ -74,8 +89,8 @@ for (lon in seq(-180, 179.5, 0.5)){ #loop in longitude
     if (length(pos_lakes)>0){  #only doing something when having a lake
       print(paste("start lon", lon, "and lat", lat))
       max_val <- max(HL_df$Lake_area[pos_lakes])
-      if(max_val>3080){  #writing data for lakes > 3080km2
-        if (which(HL_df$Lake_area[pos_lakes]>3080)>1){  #just in case, but we shouldn't have this warning
+      if(max_val>areakm2lat(lat)){  #writing data for lakes > 0.5 degrees
+        if (which(HL_df$Lake_area[pos_lakes]>areakm2lat(lat))>1){  #just in case, but we shouldn't have this warning
           print("hay dos lagos grandes OJO")
           stop("hay dos lagos grandes OJO")
         }
@@ -83,6 +98,7 @@ for (lon in seq(-180, 179.5, 0.5)){ #loop in longitude
         pos_lakes <- pos_lakes[posbig_max]
         wm_temp <- HL_df$Depth_avg[pos_lakes]
         wm_matrix[c_lat,c_lon] <- wm_temp
+        wm_biglakes[c_lat,c_lon] <- HL_df$Hylak_id[pos_lakes]
         for (v in 1:length(var_vector)){wm_list[[v]][c_lat,c_lon] <- HL_df[pos_lakes,var_vector[v]]}
       }else{  
         if (length(pos_lakes)==1){ #writing data for lonely lakes (one lake per pixel)
@@ -120,6 +136,11 @@ for (v in 1:length(var_vector)){
   crs(wm_raster) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0" 
   writeRaster(wm_raster, paste0("output/", HL_name,".tif"), overwrite=T)
 }
+#save HL id of big lakes
+wm_raster <- raster(wm_biglakes)
+extent(wm_raster) <- extent(c(-180,180,-90,90))
+crs(wm_raster) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0" 
+writeRaster(wm_raster,"output/HLid_biglakes.tif", overwrite=T)
 
 ## save shapefile with the final selected lakes
 HL_id <- wm_list[[1]][!is.na(wm_list[[1]])]
