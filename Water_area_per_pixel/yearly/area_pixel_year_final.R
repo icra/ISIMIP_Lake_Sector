b5a3e8b63130b544de57b42d63218c0a74acc837
danielmerbet <damebet@gmail.com>
2022-05-02 17:49:59 +0200

    area per pixel yearle calculation

--- Water_area_per_pixel/yearly/area_pixel_year_final.R
+++ Water_area_per_pixel/yearly/area_pixel_year_final.R
@@ -1,27 +1,21 @@
 library(raster)
-#present area per pixel map
+#present day area per pixel map
 frac <- raster("/home/dmercado/ISIMIP_Lake_Sector/Water_area_per_pixel/frac_areas.tif")
 #all years with dam construction since 1850 (included)
 years <- as.numeric(unlist(strsplit(unlist(strsplit(list.files(path="previous/",pattern="NA"), "frac_areas_NA_")), ".tif")))
 
 for (c in 1:length(years)){
   print(paste("year:", years[c]))
-  frac_temp <- raster(paste0("previous/frac_areas_", years[1],".tif"))
-  if (c>1){
-    for (y in years[2:c]){
-      frac_temp <- frac_temp + raster(paste0("previous/frac_areas_", y,".tif"))
-    }
-    rest_temp <- frac - frac_temp
-    m <- c(0, 0, NA,1,2,1)
-    rclmat <- matrix(m, ncol=3, byrow=TRUE)
-    rest_temp_NA <- reclassify(rest_temp, rclmat, include.lowest=TRUE)
-    writeRaster(rest_temp_NA,paste0("final/frac_areas_", years[c],".tif"),overwrite=T)
+  if(c==length(years)){
+    frac_NA <- raster("/home/dmercado/ISIMIP_Lake_Sector/Water_area_per_pixel/frac_areas_NA.tif")
+    writeRaster(frac_NA, paste0("final/frac_areas_", years[c],".tif"))
   }else{
-    rest_temp <- frac - frac_temp
+    for (y in years[(c+1):length(years)]){
+      rest_temp <- frac - raster(paste0("previous/frac_areas_", y,".tif"))
+    }
     m <- c(0, 0, NA,1,2,1)
     rclmat <- matrix(m, ncol=3, byrow=TRUE)
     rest_temp_NA <- reclassify(rest_temp, rclmat, include.lowest=TRUE)
-    writeRaster(rest_temp_NA,paste0("final/frac_areas_", years[c],".tif"),overwrite=T)
+    writeRaster(rest_temp_NA,paste0("final/frac_areas_NA_", years[c],".tif"), overwrite=T)
   }
-  
 }
---
+++ Water_area_per_pixel/yearly/area_pixel_year_final_BU.R
@@ -0,0 +1,34 @@
+library(raster)
+#present area per pixel map
+frac <- raster("/home/dmercado/ISIMIP_Lake_Sector/Water_area_per_pixel/frac_areas.tif")
+#all years with dam construction since 1850 (included)
+years <- as.numeric(unlist(strsplit(unlist(strsplit(list.files(path="previous/",pattern="NA"), "frac_areas_NA_")), ".tif")))
+
+for (c in 1:length(years)){
+  print(paste("year:", years[c]))
+  frac_temp <- raster(paste0("previous/frac_areas_", years[1],".tif"))
+  if (c>1){
+    for (y in years[2:c]){
+      print(y)
+      frac_temp <- frac_temp + raster(paste0("previous/frac_areas_", y,".tif"))
+    }
+    rest_temp <- frac - frac_temp
+    m <- c(0, 0, NA,1,2,1)
+    rclmat <- matrix(m, ncol=3, byrow=TRUE)
+    rest_temp_NA <- reclassify(rest_temp, rclmat, include.lowest=TRUE)
+    print(sum(frac[]))
+    print(sum(frac_temp[]))
+    print(sum(rest_temp_NA[],na.rm=T))
+    writeRaster(rest_temp_NA,paste0("final/frac_areas_", years[c],".tif"), overwrite=T)
+  }else{
+    rest_temp <- frac - frac_temp
+    m <- c(0, 0, NA,1,2,1)
+    rclmat <- matrix(m, ncol=3, byrow=TRUE)
+    rest_temp_NA <- reclassify(rest_temp, rclmat, include.lowest=TRUE)
+    print(sum(frac[]))
+    print(sum(frac_temp[]))
+    print(sum(rest_temp_NA[],na.rm=T))
+    writeRaster(rest_temp_NA,paste0("final/frac_areas_", years[c],".tif"), overwrite=T)
+  }
+  
+}
